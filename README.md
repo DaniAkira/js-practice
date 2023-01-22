648529e5482ee50ae1318cd39e4cbd6017de819a
Daniel Akira <daakira@hotmail.com>
2023-01-21 23:03:45 -0300

    Adicionando projeto que consome API viacep

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # js-practice
 Projetos acadêmicos para praticar javascript
+*Obs, não estão todos os projetos feitos, apenas os que gostei mais.
--- fetch-api/script.js
+++ fetch-api/script.js
@@ -1,21 +1,51 @@
+const input = document.querySelector(".cep-input");
+
+const results = document.querySelector(".results");
+
 const street = document.querySelector(".street-result");
 const district = document.querySelector(".district-result");
 const city = document.querySelector(".city-result");
 const state = document.querySelector(".state-result");
 
+const resetBtn = document.querySelector(".reset-btn");
+const consultBtn = document.querySelector(".consult-btn");
 
- 
+const generalDiv = document.querySelector(".general");
+const errorMessage = document.querySelector(".general p");
+
+
+
+
 let cepWhithoutHeaving = "";
 
 function consultCep() {
     const cep = document.querySelector(".cep-input").value;
     
-    if(cep.indexOf("-") === -1) {
+    if((cep.indexOf("-") === -1) && (cep.length == 8)) {
         cepWhithoutHeaving = cep
-        getData(cepWhithoutHeaving)
-    }else {
+        getData(cepWhithoutHeaving);
+        toggleBtns();
+    }else if (cep.length == 9) {
         replaceHeaving(cep);
+        getData(cepWhithoutHeaving);
+        toggleBtns();
+    }else {
+        showErrorFormat(); 
+        toggleBtns();
+    }
+}
 
+function resetCep() {
+    const errorMessage = generalDiv.querySelector(".error-message");
+
+    if (errorMessage != undefined && errorMessage != null){
+        input.value = "";
+        toggleBtns();
+        removeErrorMessage();
+    } else {
+        input.value = "";
+        toggleBtns();
+        toggleResults();
     }
 }
 
@@ -29,7 +59,13 @@ async function getData(cep){
             .then(response => {
                 return response.json()
             }).then(data => {
-                showResults(data)
+                if (data.erro == true){
+                    showError();
+                    console.log(`${typeof(errorMessage)} OIOI`)
+                }else {
+                    showResults(data);
+                    toggleResults();
+                }
         })    
 }
 
@@ -39,4 +75,39 @@ function showResults(data) {
     city.innerHTML = data.localidade
     state.innerHTML = data.uf
 }
+
+function toggleResults() {
+    results.classList.toggle("hide");
+}
+
+function toggleBtns() {
+    resetBtn.classList.toggle("hide");
+    consultBtn.classList.toggle("hide");
+}
+
+function showError() {
+    const errorMessage = `Endereço não existe em nosso banco de dados`;
+    const errorHtml = document.createElement("p");
+
+    errorHtml.classList.add("error-message");
+    errorHtml.innerHTML = errorMessage;
+
+    generalDiv.appendChild(errorHtml);
+}
+
+function showErrorFormat() {
+    const errorMessage = `Formato inválido, digite o CEP nos formatos "00000000" ou "00000-000"`
+    const errorHtml = document.createElement("p");
+
+    errorHtml.classList.add("error-message");
+    errorHtml.innerHTML = errorMessage;
+
+    generalDiv.appendChild(errorHtml);
+}
+
+function removeErrorMessage() {
+    const errorText = document.querySelector(".error-message");
+    errorText.remove();
+}
+
 
